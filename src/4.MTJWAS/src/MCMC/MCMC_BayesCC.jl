00a73f9bc1decffe0db2aa6c558c462cfce7dcd9
Hao Cheng <reworkhow@gmail.com>
2016-06-30 14:02:51 -0500

    update sampling pi in BayesCC

--- src/4.MTJWAS/src/MCMC/MCMC_BayesCC.jl
+++ src/4.MTJWAS/src/MCMC/MCMC_BayesCC.jl
@@ -132,21 +132,24 @@ function MCMC_BayesCC(nIter,mme,df,Pi;
         end
 
         iloci = 1
-        nLoci_array=zeros(2^nTraits)
-        for i in keys(BigPi) #assume order of key won't change
+        nLoci_array=zeros(BigPi)
+        for i in labels
           temp2 = broadcast(-,temp,i')
           nLoci =  sum(mean(abs(temp2),2).==0.0)
           nLoci_array[iloci] = nLoci +1
           iloci = iloci +1
         end
-        tempPi = rand(Dirichlet(nLoci_array))
-
-        iloci = 1
-        for i in keys(BigPi)
-          BigPi[i] = tempPi[iloci]
-          BigPiMean[i] += (tempPi[iloci]-BigPiMean[i])/iter
-          iloci = iloci +1
-        end
+        
+        BigPi = rand(Dirichlet(nLoci_array))
+        BigPiMean += (BigPi-BigPiMean)/iter
+        
+        #tempPi = rand(Dirichlet(nLoci_array))
+        #iloci = 1
+        #for i in keys(BigPi)
+        #  BigPi[i] = tempPi[iloci]
+        #  BigPiMean[i] += (tempPi[iloci]-BigPiMean[i])/iter
+        #  iloci = iloci +1
+        #end
 
         #####################################
         #sample residual covariance matrix
