51c77679f1b4a0172b4a03b497f83f7e0b097135
Hao Cheng <reworkhow@gmail.com>
2016-07-08 14:29:28 -0500

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,8 @@ using JWAS
 using Base.Test
 
 # write your own tests here
-@test
-model_equations = "BW = intercept + age + sex;
-                   CW = intercept + age + sex";
-R               = [6.72   24.84
-                   24.84  708.41]
-models          = JWAS.MT.build_model(model_equations,R);
+@test model_equations = "BW = intercept + age + sex;
+                         CW = intercept + age + sex";
+@test R               = [6.72   24.84
+                         24.84  708.41]
+@test models          = JWAS.MT.build_model(model_equations,R);
