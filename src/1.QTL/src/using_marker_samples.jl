206d00c971b76d741e21ec45d2defcc2231ff825
Hao Cheng <reworkhow@gmail.com>
2016-07-14 16:26:14 -0500

    make genetic variance and ebv from marker samples

---
+++ src/1.QTL/src/using_marker_samples.jl
@@ -0,0 +1,88 @@
+function get_BV_samples(M::Array{Float64,2},marker_file;header=true)
+    if header==true
+        alpha=readdlm(marker_file,header=true)[1]
+    else
+        alpha=readdlm(marker_file)
+    end
+    M*alpha'
+end
+
+function get_breeding_values(M::Array{Float64,2},files...)
+    nTraits = length(files)
+    BV  = Array(Array{Float64,2},nTraits)
+    traiti=1
+    
+    EBV = Array(Any,nTraits)
+    for i in files
+        BV[traiti] = get_BV_samples(M,i)
+        EBV[traiti]= DataFrame(EBV=vec(mean(BV[traiti],2)),PEV=vec(var(BV[traiti],2)))
+        traiti +=1
+    end  
+    return EBV
+end 
+
+function get_additive_genetic_variances(M::Array{Float64,2},files...)
+    nTraits = length(files)
+    BV  = Array(Array{Float64,2},nTraits)
+    traiti=1
+    for i in files
+        BV[traiti] = get_BV_samples(M,i)
+        traiti +=1
+    end
+    
+    num_samples = size(BV[1],2)   
+    G = Array(Any,num_samples)
+    for i = 1:num_samples
+        BVi = vec(BV[1][:,i])
+        for j = 2:nTraits
+            BVi = [BVi BV[j][:,i]]
+        end  
+        G[i] = cov(BVi)
+    end
+    Gmean = mean(G)
+    Gvar  = mean(G.^2)-Gmean.^2
+    return G,Gmean,Gvar
+end  
+
+
+function get_BV_samples(M::Array{Float64,2},marker_file;header=true)
+    if header==true
+        alpha=readdlm(marker_file,header=true)[1]
+    else
+        alpha=readdlm(marker_file)
+    end
+    M*alpha'
+end
+
+function get_breeding_values(M::Array{Float64,2},files...)
+    nTraits = length(files)
+    BV  = Array(Array{Float64,2},nTraits)
+    traiti=1
+    
+    EBV = Array(Any,nTraits)
+    for i in files
+        BV[traiti] = get_BV_samples(M,i)
+        EBV[traiti]= DataFrame(EBV=vec(mean(BV[traiti],2)),PEV=vec(var(BV[traiti],2)))
+        traiti +=1
+    end  
+    return EBV
+end 
+
+function get_additive_genetic_variances(model,files...)
+    return get_additive_genetic_variances(model.M.genotypes,files...)
+end 
+
+function get_breeding_values(model,files...)
+    EBV=get_breeding_values(M::Array{Float64,2},files...)
+    if model.M.obsID[1] != "NA"       
+        EBVwithID = Any(EBV)
+        traiti=1
+        for i in EBV
+            ID        = DataFrame(ID=model.M.obsID)
+            EBVwithID[traiti] = [ID i]
+            traiti +=1 
+        end
+        return EBVwithID       
+    end
+    return EBV      
+end
