b9a5778aec1a26b3e3f0c7964c2ed25f3f4c9913
Hao Cheng <reworkhow@gmail.com>
2016-07-26 14:36:34 -0500

    Update MCMC_BayesCC.jl

--- src/4.MTJWAS/src/MCMC/MCMC_BayesCC.jl
+++ src/4.MTJWAS/src/MCMC/MCMC_BayesCC.jl
@@ -184,7 +184,7 @@ function MCMC_BayesCC(nIter,mme,df,Pi;
         if constraint == true
           R0 = zeros(nTraits,nTraits)
           for traiti = 1:nTraits
-            R0[i,i]= (SRes[traiti,traiti]+ν*scaleRes[traiti])/rand(Chisq(nObs+ν))
+            R0[traiti,traiti]= (SRes[traiti,traiti]+ν*scaleRes[traiti])/rand(Chisq(nObs+ν))
           end
         end
 
