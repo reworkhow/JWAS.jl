b134d79ae64b54b387fbd7a758164948eedb060b
Hao Cheng <reworkhow@gmail.com>
2016-08-04 15:06:16 -0500

    fix a bug in optional argument in solver

--- src/4.MTJWAS/src/solver.jl
+++ src/4.MTJWAS/src/solver.jl
@@ -1,4 +1,4 @@
-function Jacobi(A,x,b,p;tol=0.000001,output=10)
+function Jacobi(A,x,b,p;tolerance=0.000001,output=10)
     D       = diag(A)
     res     = A*x
     resid   = b-res
@@ -6,7 +6,7 @@ function Jacobi(A,x,b,p;tol=0.000001,output=10)
     diff    = sum(resid.^2)
     n    = size(A,1)
     iter = 0
-    while ((diff/n > tol) & (iter<1000))
+    while ((diff/n > tolerance) & (iter<1000))
         iter += 1
         x = p*tempSol + (1-p)*x
         res     = A*x
@@ -20,14 +20,14 @@ function Jacobi(A,x,b,p;tol=0.000001,output=10)
     return x
 end
 
-function GaussSeidel(A,x,b;tol=0.000001,output=10)
+function GaussSeidel(A,x,b;tolerance=0.000001,output=10)
     n = size(x,1)
     for i=1:n
         x[i] = ((b[i] - A[i,:]*x)/A[i,i])[1,1] + x[i]
     end
     diff = sum((A*x-b).^2)
     iter = 0
-    while ((diff/n > tol) & (iter<1000))
+    while ((diff/n > tolerance) & (iter<1000))
         iter += 1
         for i=1:n
             x[i] = ((b[i] - A[i,:]*x)/A[i,i])[1,1] + x[i]
