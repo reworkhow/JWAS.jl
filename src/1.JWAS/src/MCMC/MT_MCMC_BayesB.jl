38e658bea5125b16814592cc9fbe6c527dd3a587
haocheng <reworkhow@gmail.com>
2017-03-28 08:30:33 -0500

    update MTBayeB to allow estimating \Pi

--- src/1.JWAS/src/MCMC/MT_MCMC_BayesB.jl
+++ src/1.JWAS/src/MCMC/MT_MCMC_BayesB.jl
@@ -1,5 +1,6 @@
 function MT_MCMC_BayesB(nIter,mme,df,Pi;
                       sol=false,outFreq=100,
+                      estimatePi        =false,
                       missing_phenotypes=false,
                       constraint=nothing,
                       output_samples_frequency=0)
@@ -138,29 +139,9 @@ function MT_MCMC_BayesB(nIter,mme,df,Pi;
                                    uArray,meanuArray,
                                    iR0,iGM,iter,BigPi)
 
-        #=
-        temp = deltaArray[1]
-        for traiti = 2:nTraits
-          temp = [temp deltaArray[traiti]]
-        end
-
-        iloci = 1
-        nLoci_array=zeros(2^nTraits)
-        for i in keys(BigPi) #assume order of key won't change
-          temp2 = broadcast(-,temp,i')
-          nLoci =  sum(mean(abs(temp2),2).==0.0)
-          nLoci_array[iloci] = nLoci +1
-          iloci = iloci +1
-        end
-        tempPi = rand(Dirichlet(nLoci_array))
-
-        iloci = 1
-        for i in keys(BigPi)
-          BigPi[i] = tempPi[iloci]
-          BigPiMean[i] += (tempPi[iloci]-BigPiMean[i])/iter
-          iloci = iloci +1
-        end
-        =#
+       if estimatePi == true
+         samplePi(deltaArray,BigPi,BigPiMean,iter)
+       end
 
         #####################################
         #sample residual covariance matrix
