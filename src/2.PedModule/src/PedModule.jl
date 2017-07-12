bb804e7b16e67de3aa2639e744378ab8f632330d
haocheng <reworkhow@gmail.com>
2017-07-11 22:43:23 -0700

    fix warning caused by 0.6
    Array(String,n)

--- src/2.PedModule/src/PedModule.jl
+++ src/2.PedModule/src/PedModule.jl
@@ -185,7 +185,7 @@ end
 
 function getIDs(ped::Pedigree)
     n = length(ped.idMap)
-    ids = Array(String,n)
+    ids = Array{String}(n)
     for i in ped.idMap
       ids[i[2].seqID] = i[1]
     end
