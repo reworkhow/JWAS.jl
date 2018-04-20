4de3d28d65f1a7f90945ae3822f3f2fddcc2e332
Hao Cheng <reworkhow@gmail.com>
2018-04-19 18:49:50 -0700

    fix a bug in make.jl for documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -16,8 +16,6 @@ makedocs(
             "man/examples.md",
             "Contributing" => "examples/genomicBLMM.md",
             ],
-
-        ],
         "Examples" => Any[
             "Linear Mixed Model (conventional)" => "examples/conventionalBLMM.md",
             "Linear Additive Genetic Model" => "examples/LinearAdditiveGeneticModel.md",
@@ -29,21 +27,6 @@ makedocs(
             # hide("Internals" => "lib/internals.md", Any[
             #     "lib/internals/anchors.md",
             #     "lib/internals/builder.md",
-            #     "lib/internals/cross-references.md",
-            #     "lib/internals/docchecks.md",
-            #     "lib/internals/docsystem.md",
-            #     "lib/internals/documenter.md",
-            #     "lib/internals/documents.md",
-            #     "lib/internals/dom.md",
-            #     "lib/internals/expanders.md",
-            #     "lib/internals/formats.md",
-            #     "lib/internals/generator.md",
-            #     "lib/internals/mdflatten.md",
-            #     "lib/internals/selectors.md",
-            #     "lib/internals/textdiff.md",
-            #     "lib/internals/utilities.md",
-            #     "lib/internals/walkers.md",
-            #     "lib/internals/writers.md",
             #])
         ]
     ],
