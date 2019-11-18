c15667b015cbfc460053fd28d673394ebb039984
qtlchengadmin <reworkhow@gmail.com>
2019-11-18 13:19:22 -0800

    merge single-trait BayesB and BayesC

--- src/1.JWAS/src/markers/BayesianAlphabet/BayesABC.jl
+++ src/1.JWAS/src/markers/BayesianAlphabet/BayesABC.jl
@@ -1,36 +1,39 @@
-function sampleEffectsBayesC!(xArray,
+function sampleEffectsBayesABC!(xArray,
                               xpx, #this is XpRinvX
                               yCorr,
                               α,
+                              β,
                               δ,
                               vare,
                               varEffects,
                               π,
-                              Rinv)	#Heterogeneous residuals
+                              invweights)	#Heterogeneous residuals
 
     logPi         = log(π)
     logPiComp     = log(1.0-π)
-    logVarEffects = log(varEffects)
     logDelta0     = logPi
     invVarRes     = 1.0/vare
-    invVarEffects = 1.0/varEffects
+    invVarEffects = 1.0./varEffects
+    logVarEffects = log.(varEffects)
     nLoci         = 0
     nMarkers      = length(α)
 
-    yCorr = yCorr.*Rinv
+    #yCorr = yCorr.*invweights
+
     for j=1:nMarkers
         x = xArray[j]
         rhs = (dot(x,yCorr) + xpx[j]*α[j])*invVarRes
-        lhs = xpx[j]*invVarRes + invVarEffects
+        lhs = xpx[j]*invVarRes + invVarEffects[j]
         invLhs = 1.0/lhs
         gHat   = rhs*invLhs
-        logDelta1  = -0.5*(log(lhs) + logVarEffects - gHat*rhs) + logPiComp
+        logDelta1  = -0.5*(log(lhs) + logVarEffects[j] - gHat*rhs) + logPiComp
         probDelta1 = 1.0/(1.0 + exp(logDelta0 - logDelta1))
         oldAlpha = α[j]
 
         if(rand()<probDelta1)
             δ[j] = 1
-            α[j] = gHat + randn()*sqrt(invLhs)
+            β[j] = gHat + randn()*sqrt(invLhs)
+            α[j] = β[j]
             BLAS.axpy!(oldAlpha-α[j],x,yCorr)
             nLoci = nLoci + 1
         else
@@ -38,6 +41,7 @@ function sampleEffectsBayesC!(xArray,
                 BLAS.axpy!(oldAlpha,x,yCorr)
             end
             δ[j] = 0
+            β[j] = randn()*sqrt(varEffects[j])
             α[j] = 0
         end
     end
