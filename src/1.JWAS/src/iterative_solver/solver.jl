e7aae2d015903c0f8f807db0ba698f3625280422
qtlchengadmin <reworkhow@gmail.com>
2020-09-24 16:20:30 -0700

    test zero diagonals in MME

--- src/1.JWAS/src/iterative_solver/solver.jl
+++ src/1.JWAS/src/iterative_solver/solver.jl
@@ -126,9 +126,11 @@ function Gibbs(A,x,b,niter::Int64;printout_frequency=100)
             println("at iteration: ",iter)
         end
         for i=1:n
-            invlhs = 1.0/A[i,i]
-            μ      = invlhs*(b[i] - A[:,i]'x) + x[i]
-            x[i]   = randn()*sqrt(invlhs) + μ
+            if A[i,i] != 0.0 #issue70, zero diagonals in MME
+                invlhs = 1.0/A[i,i]
+                μ      = invlhs*(b[i] - A[:,i]'x) + x[i]
+                x[i]   = randn()*sqrt(invlhs) + μ
+            end
         end
         xmean += (x - xmean)/iter
     end
@@ -138,17 +140,21 @@ end
 #one iteration of Gibbs sampler for lambda version of MME (single-trait)
 function Gibbs(A,x,b,vare::AbstractFloat)
     for i = 1:size(x,1)
-        invlhs  = 1.0/A[i,i]
-        μ       = invlhs*(b[i] - A[:,i]'x) + x[i]
-        x[i]    = randn()*sqrt(invlhs*vare) + μ
+        if A[i,i] != 0.0 #issue70, zero diagonals in MME
+            invlhs  = 1.0/A[i,i]
+            μ       = invlhs*(b[i] - A[:,i]'x) + x[i]
+            x[i]    = randn()*sqrt(invlhs*vare) + μ
+        end
     end
 end
 
 #one iteration of Gibbs sampler for general version of MME (multi-trait)
 function Gibbs(A,x,b)
     for i = 1:size(x,1)
-        invlhs  = 1.0/A[i,i]
-        μ       = invlhs*(b[i] - A[:,i]'x) + x[i]
-        x[i]    = randn()*sqrt(invlhs) + μ
+        if A[i,i] != 0.0 #issue70, zero diagonals in MME
+            invlhs  = 1.0/A[i,i]
+            μ       = invlhs*(b[i] - A[:,i]'x) + x[i]
+            x[i]    = randn()*sqrt(invlhs) + μ
+        end
     end
 end
