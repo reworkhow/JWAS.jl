1888ed972204eca8c17124f9abe1a75f0024e829
qtlchengadmin <reworkhow@gmail.com>
2020-11-17 20:25:51 -0800

    fix a bug (use of sd instead of variance in rand(Normal))

--- src/1.JWAS/src/censored_trait/censored_trait.jl
+++ src/1.JWAS/src/censored_trait/censored_trait.jl
@@ -5,7 +5,8 @@ function censored_trait_setup!(mme)
     starting_value = mme.sol
     cmean          = mme.X*starting_value[1:size(mme.mmeLhs,1)] #maker effects defaulting to all zeros
     for i in 1:length(mme.ySparse) #1,2,2,3,1...
-        mme.ySparse[i] = rand(truncated(Normal(cmean[i], mme.R), lower_bound[i], upper_bound[i]))
+        #mme.ySparse[i] = rand(truncated(Normal(cmean[i], sqrt(mme.R)), lower_bound[i], upper_bound[i]))
+        mme.ySparse[i] = rand((lower_bound[i]:upper_bound[i]))
     end
     return lower_bound,upper_bound
 end
@@ -16,7 +17,7 @@ function censored_trait_sample_liabilities(mme,ycorr,lower_bound,upper_bound)
     ########################################################################
     cmean = mme.ySparse - ycorr #liabilities - residuals
     for i in 1:length(mme.ySparse) #1,2,2,3,1...
-        mme.ySparse[i] = rand(truncated(Normal(cmean[i], mme.R), lower_bound[i], upper_bound[i]))
+        mme.ySparse[i] = rand(truncated(Normal(cmean[i], sqrt(mme.R)), lower_bound[i], upper_bound[i]))
     end
     ycorr = mme.ySparse - cmean
     return ycorr
