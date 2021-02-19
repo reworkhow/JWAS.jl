93c1da8ee63f3424f42e895c25dcf921c67e9d94
qtlchengadmin <reworkhow@gmail.com>
2021-02-19 12:20:38 -0800

    update simulatoin code

--- src/4.Datasets/src/simulation_realgeno.jl
+++ src/4.Datasets/src/simulation_realgeno.jl
@@ -13,22 +13,27 @@ using DelimitedFiles,JWAS, Statistics, Distributions, DataFrames, LinearAlgebra,
 using Random
 Random.seed!(314);
 
+#load genotype and pedigree
 M            = readdlm("genotypes.csv",',');
 pedigree     = CSV.read("pedigree.csv",DataFrame)
 ped          = get_pedigree("pedigree.csv",header=true);
 IDs,Ai,inbred= get_info(ped,Ai=true);
 A            = Symmetric(inv(Matrix(Ai))); #numerator relationship matrix
 
+#data simulation
 data      = DataFrame(ID=M[2:end,1])
-data[:x1] = round.(randn(size(data,1)),digits=2)
-data[:x2] = sample(["g1","g2","g3","g4","g5"],size(data,1))
-data[:x3] = sample(["m","f"],size(data,1));
+data[!,:x1] = round.(randn(size(data,1)),digits=2)
+data[!,:x2] = sample(["g1","g2","g3","g4","g5"],size(data,1))
+data[!,:x3] = sample(["m","f"],size(data,1));
 data      = leftjoin(data, pedigree[!,[:ID,:dam]], on = :ID)
 data
 
+#Genetic/Phenotypic Value Simulation
+##simulation parameters
 nQTL = 10
 h2   = [0.7,0.5,0.2];
 
+##genomic info
 QTL_covariates = M[2:end,rand(2:size(M,2),nQTL)];
 QTL_effects    = rand(MvNormal(zeros(3), [1.0 0.5 0.3
                                           0.5 1.0 0.5
@@ -39,22 +44,23 @@ bv1 = BV[:,1]./std(BV[:,1])*sqrt(h2[1])
 bv2 = BV[:,2]./std(BV[:,2])*sqrt(h2[2])
 bv3 = BV[:,3]./std(BV[:,3])*sqrt(h2[3])
 println("variance for bv1, bv2, and bv3 are: $(var(bv1)), $(var(bv2)), $(var(bv3))")
-
+##maternal effects
 dam_effect = rand(MvNormal(zeros(length(IDs)), A*sqrt(0.1)))
-dam        = JWAS.mkmat_incidence_factor(data[:dam],[IDs;missing])*[dam_effect;0.0];
+dam        = JWAS.mkmat_incidence_factor(data[!,:dam],[IDs;missing])*[dam_effect;0.0];
 
+##non-genetic effects
 x1_effect,x2_effect,x3_effect  = 1.0, randn(5), randn(2)
-x1 = data[:x1]*x1_effect
-x2 = JWAS.mkmat_incidence_factor(data[:x2],["g1","g2","g3","g4","g5"])*x2_effect
-x3 = JWAS.mkmat_incidence_factor(data[:x3],["m","f"])*x3_effect;
+x1 = data[!,:x1]*x1_effect
+x2 = JWAS.mkmat_incidence_factor(data[!,:x2],["g1","g2","g3","g4","g5"])*x2_effect
+x3 = JWAS.mkmat_incidence_factor(data[!,:x3],["m","f"])*x3_effect;
+##genetic/phenotypic value
+data[!,:bv1] = bv1 + dam
+data[!,:bv2] = bv2
+data[!,:bv3] = bv3;
 
-data[:bv1] = bv1 + dam
-data[:bv2] = bv2
-data[:bv3] = bv3;
-
-data[:y1] = data[:bv1] + x1 + x2 + x2.*x3 + randn(size(data,1))*(1-h2[1]-0.1)
-data[:y2] = data[:bv2] + x1 + x2 + randn(size(data,1))*(1-h2[2])
-data[:y3] = data[:bv3] + x1 + randn(size(data,1))*(1-h2[3]);
+data[!,:y1] = data[!,:bv1] + x1 + x2 + x2.*x3 + randn(size(data,1))*(1-h2[1]-0.1)
+data[!,:y2] = data[!,:bv2] + x1 + x2 + randn(size(data,1))*(1-h2[2])
+data[!,:y3] = data[!,:bv3] + x1 + randn(size(data,1))*(1-h2[3]);
 
 data=data[!,[:ID,:y1,:y2,:y3,:x1,:x2,:x3,:dam,:bv1,:bv2,:bv3]]
 
