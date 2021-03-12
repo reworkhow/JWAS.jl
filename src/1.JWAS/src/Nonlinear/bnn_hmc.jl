f0a27eaa87437a018ba9e1a6a120071b6f62ce6d
tj <tjzhao@ucdavis.edu>
2021-03-12 14:45:52 -0800

    remove redundent parameters in helper functions

--- src/1.JWAS/src/Nonlinear/bnn_hmc.jl
+++ src/1.JWAS/src/Nonlinear/bnn_hmc.jl
@@ -30,7 +30,9 @@
 
 
 #helper 1: calculate gradiant of all latent traits for all individual
-function calc_gradient_z(Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr)
+function calc_gradient_z(Z,y,mu_W1,Sigma2z,sigma2e,ycorr)
+    mu=mu_W1[1]
+    W1=mu_W1[2:end]
     n=length(y)
     tanhZ=tanh.(Z)
     #dlogfz =- (Z - ones(n)*Mu0' - X*W0) * inv(Sigma2z)  #(n,l1)
@@ -43,8 +45,9 @@ end
 
 
 # helper 2: calculate log p(z|y) to help calculate the acceptance rate
-function calc_log_p_z(Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr)
-
+function calc_log_p_z(Z,y,mu_W1,Sigma2z,sigma2e,ycorr)
+    mu=mu_W1[1]
+    W1=mu_W1[2:end]
     n=length(y)
     #logfz = -0.5*sum(((Z-ones(n)*Mu0'-X*W0).^2)*inv(Sigma2z),dims=2) .- (0.5*log(prod(diag(Sigma2z))))
     logfz = -0.5*sum((ycorr.^2)*inv(Sigma2z),dims=2) .- (0.5*log(prod(diag(Sigma2z))))  #
@@ -57,31 +60,30 @@ end
 
 
 #helper 3: one iterations of HMC to sample Z
-function hmc_one_iteration(nLeapfrog,epsilon,Z,y,W0,mu_W1,Sigma2z,sigma2e,Mu0,ycorr)
-    mu=mu_W1[1]
-    W1=mu_W1[2:end]
+function hmc_one_iteration(nLeapfrog,epsilon,Z,y,mu_W1,Sigma2z,sigma2e,ycorr)
+
     n,l1 = size(Z)
     old_Z = Matrix(Z)
 
     #Step1. update phi ~ N(0,M)
     phi = randn(n,l1)  #rand(n,Normal(0,sigma2phi))
-    log_p_old = calc_log_p_z(old_Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr) - 0.5*sum(phi.^2,dims=2)  #(n,1)
+    log_p_old = calc_log_p_z(old_Z,y,mu_W1,Sigma2z,sigma2e,ycorr) - 0.5*sum(phi.^2,dims=2)  #(n,1)
     #Step2. update (zl,phi) from 10 leapfrog
-    phi += 0.5 * epsilon * calc_gradient_z(Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr)  #(n,l1)
+    phi += 0.5 * epsilon * calc_gradient_z(Z,y,mu_W1,Sigma2z,sigma2e,ycorr)  #(n,l1)
     for leap_i in 1:nLeapfrog
        #(b) full step of theta
        Z += epsilon * phi  # (n,l1)
        ycorr += epsilon * phi #update ycorr due to change of Z
        #(c) half step of phi
        if leap_i==nLeapfrog
-           phi += 0.5 * epsilon * calc_gradient_z(Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr)
+           phi += 0.5 * epsilon * calc_gradient_z(Z,y,mu_W1,Sigma2z,sigma2e,ycorr)
        else
-           phi += epsilon * calc_gradient_z(Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr)
+           phi += epsilon * calc_gradient_z(Z,y,mu_W1,Sigma2z,sigma2e,ycorr)
        end
     end
 
     #Step3. acceptance rate
-    log_p_new = calc_log_p_z(Z,y,W0,W1,Sigma2z,sigma2e,Mu0,mu,ycorr) - 0.5*sum(phi.^2,dims=2) #(n,1)
+    log_p_new = calc_log_p_z(Z,y,mu_W1,Sigma2z,sigma2e,ycorr) - 0.5*sum(phi.^2,dims=2) #(n,1)
     r         = exp.(log_p_new - log_p_old)  # (n,1)
     nojump = rand(n) .> r  # bool (n,1)
 
