3ec5971cb0266aca99e4fd221366bf9b6bea3714
tj <tjzhao@ucdavis.edu>
2022-02-11 09:22:44 -0800

    MT: allow multiple censored traits + multiple/none continuouse traits

--- src/1.JWAS/src/censored_trait/censored_trait.jl
+++ src/1.JWAS/src/censored_trait/censored_trait.jl
@@ -17,44 +17,42 @@ function censored_trait_setup!(mme)
     return lower_bound,upper_bound
 end
 
-#### sample liabilities
+
 function MT_censored_trait_sample_liabilities(mme,ycorr,lower_bound,upper_bound)
     ########################################################################
-    # MT: ONLY for one censored trait + one continuous trait
+    # MT: multiple censored traits + multiple/none continuose traits
     # continouse traits: UB=LB=y
     ########################################################################
     cmean = mme.ySparse - ycorr #liabilities - residuals
     nInd=length(mme.obsID)
     nTrait=mme.nModels
-    cmean = reshape(cmean,nInd,nTrait)
-    lower_bound = reshape(lower_bound,nInd,nTrait)
-    upper_bound = reshape(upper_bound,nInd,nTrait)
-    ySparse = reshape(mme.ySparse,nInd,nTrait)
+    cmean       = reshape(cmean,       nInd,nTrait)
+    lower_bound = reshape(lower_bound, nInd,nTrait)
+    upper_bound = reshape(upper_bound, nInd,nTrait)
+    ySparse     = reshape(mme.ySparse, nInd,nTrait)
     R=mme.R
 
+    #### sample liabilities
     for i in 1:nInd
-        if lower_bound[i,:] == upper_bound[i,:] #two traits are continuous; trait1_lb=trait1_ub && trait2_lb=trait2_ub
-            ySparse[i,:] = lower_bound[i,:]
-        else #one trait is censored: sample censored traits | continuouse trait
-            for t in 1:nTrait
-                if lower_bound[i,t] == upper_bound[i,t] # t-th trait is continuous
-                    ySparse[i,t] = lower_bound[i,t]
-                else # t-th trait is censored, use "1" denote censored trait, "2" denote continuous trait
-                     # residual for continuous trait = d
-                    index1=[t]  #index for censored trait
-                    index2=deleteat!(collect(1:2),t) #index for continuous trait
-                    d=ySparse[i,index2]-cmean[i,index2]
-                    #sample residual from its conditional distribution
-                    μ_1 = R[index1,index2]*inv(R[index2,index2])*d
-                    σ2_1= R[index1,index1]-R[index1,index2]*inv(R[index2,index2])*R[index2,index1]
-                    ϵ1_lower_bound=lower_bound[i,t]-cmean[i,t]
-                    ϵ1_upper_bound=upper_bound[i,t]-cmean[i,t]
-                    μ_1=μ_1[1]   #a vector of length 1
-                    σ2_1=σ2_1[1] #a vector of length 1
-                    ϵ1 = rand(truncated(Normal(μ_1,sqrt(σ2_1)), ϵ1_lower_bound, ϵ1_upper_bound))
-                    #update ySparse
-                    ySparse[i,t] = cmean[i,t] + ϵ1
-                end
+        #sample liabilities for each traits if needed
+        for t in 1:nTrait
+            if lower_bound[i,t] == upper_bound[i,t] # t-th trait is continuous
+                ySparse[i,t] = lower_bound[i,t]
+            else # t-th trait is censored; use "1" denote censored trait, "2" denote all other traits
+                 # residual for all other traits traits = d
+                index1=[t]  #index for the censored trait
+                index2=deleteat!(collect(1:2),t) #index for all other traits
+                d=ySparse[i,index2]-cmean[i,index2]
+                # sample residual for censored trait "1"
+                μ_1 = R[index1,index2]*inv(R[index2,index2])*d
+                σ2_1= R[index1,index1]-R[index1,index2]*inv(R[index2,index2])*R[index2,index1]
+                ϵ1_lower_bound=lower_bound[i,t]-cmean[i,t]
+                ϵ1_upper_bound=upper_bound[i,t]-cmean[i,t]
+                μ_1=μ_1[1]   #extract value from a vector of length 1
+                σ2_1=σ2_1[1] #extract value from a vector of length 1
+                ϵ1 = rand(truncated(Normal(μ_1,sqrt(σ2_1)), ϵ1_lower_bound, ϵ1_upper_bound))
+                #update ySparse
+                ySparse[i,t] = cmean[i,t] + ϵ1
             end
         end
     end
