afd244b506a4660a14a29901e2e6061adaecb16f
Hao Cheng <reworkhow@gmail.com>
2022-07-21 10:56:10 -0700

    fix another typo in make for doc

--- docs/make.jl
+++ docs/make.jl
@@ -20,11 +20,11 @@ makedocs(
             "Internals" => "manual/internals.md"
             ],
         "Mixed Effects Neural Networks (NNMM)" => Any[
-                    "Part1.introduction" => "Part1_introduction.md"
-                    "Part2.NNMM" => "Part2_NNMM.md"
-                    "Part3.NNMM with intermediate omics" => "Part3_NNMMwithIntermediateOmics.md"
-                    "Part4.partial connected neural network" => "Part4_PartialConnectedNeuralNetwork.md"
-                    "Part5.user-defined onlinear function" => "Part5_UserDefinedNonlinearFunction.md"
+                    "Part 1. introduction" => "nnmm/Part1_introduction.md"
+                    "Part 2. NNMM" => "nnmm/Part2_NNMM.md"
+                    "Part 3. NNMM with intermediate omics" => "nnmm/Part3_NNMMwithIntermediateOmics.md"
+                    "Part 4. partial connected neural network" => "nnmm/Part4_PartialConnectedNeuralNetwork.md"
+                    "Part 5. user-defined onlinear function" => "nnmm/Part5_UserDefinedNonlinearFunction.md"
                     ],
         "Examples" => Any[
             "Examples" => "examples/examples.md"
