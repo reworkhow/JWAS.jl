ddef548df67d69ab323a788dead11b084c32842a
Jiayi Qu <jyqqu@ucdavis.edu>
2022-07-22 14:39:40 -0700

    modified unitests

--- src/4.Datasets/src/Datasets.jl
+++ src/4.Datasets/src/Datasets.jl
@@ -1,6 +1,6 @@
 module Datasets
 
-using Printf
+using Printf,HTTP
 function dataset(file_name::AbstractString;dataset_name::AbstractString="")
     basename = joinpath(dirname(@__FILE__), "..", "data", dataset_name)
     rdaname = joinpath(basename, string(file_name))
@@ -11,6 +11,13 @@ function dataset(file_name::AbstractString;dataset_name::AbstractString="")
     end
 end
 
+function dataset_big(file_name::AbstractString;dataset_url::AbstractString="https://raw.githubusercontent.com/zhaotianjing/bio_protocol/main/data")
+    rdaname = joinpath(dataset_url, string(file_name))
+    http_obj = HTTP.get(rdaname)
+    return http_obj
+    end
+
 export dataset
+
 
 end # module
---
+++ test/test_BayesianAlphabet_PA.jl
@@ -0,0 +1,157 @@
+using CSV,DataFrames,JWAS,JWAS.Datasets,Statistics
+phenofile     = Datasets.dataset_big("phenotypes.txt")
+pedfile       = Datasets.dataset_big("pedigree.txt")
+genofile      = Datasets.dataset_big("genotypes_5k.txt")
+genofile_ssbr = Datasets.dataset_big("genotypes_2kind.txt")
+#mapfile    = Datasets.dataset_big("map.txt")
+
+phenotypes        = CSV.read(phenofile.body,DataFrame,delim = ',',header=true,missingstrings=["."])
+phenotypes[!,:dam]=phenotypes[!,:ID]
+pedfile           = CSV.read(pedfile.body,DataFrame,delim = ',',header=true,missingstrings=["."])
+pedigree          = get_pedigree(pedfile,separator=",",header=true);
+genofile          = CSV.read(genofile.body,DataFrame,delim = ',',header=true,missingstrings=["."])
+genofile_ssbr     = CSV.read(genofile_ssbr.body,DataFrame,delim = ',',header=true,missingstrings=["."])
+
+if ispath("mytest") == true
+      rm("mytest", recursive=true)
+end
+mkdir("mytest/")
+cd("mytest/")
+
+PA_dict = Dict{String,Float64}()
+
+
+for single_step in [false,true]
+      for test_method in ["BayesA","BayesB","BayesC","RR-BLUP","BayesL","GBLUP", "PBLUP"]
+            # ST analysis
+            newdir = "ST_"*(single_step ? "SS" : "")*test_method*"/"
+            mkdir(newdir)
+            cd(newdir)
+            if test_method in ["BayesC","BayesB"]
+                  test_estimatePi = true
+            else
+                  test_estimatePi = false
+            end
+
+            printstyled("\n\n\n\n\n\n\n\nTest single-trait $test_method analysis using $(single_step ? "in" : "")complete genomic data\n\n\n",bold=true,color=:green)
+            if test_method != "PBLUP"
+                  G3 =1.0
+                  if single_step
+                        global geno = get_genotypes(genofile_ssbr,G3,header=true,separator=',',method=test_method,estimatePi=test_estimatePi);
+                  else
+                        global geno = get_genotypes(genofile,G3,header=true,separator=',',method=test_method,estimatePi=test_estimatePi);
+                  end
+            end
+
+            if test_method != "PBLUP"
+                  model_equation1  ="t1 = intercept + ID + dam + geno";
+            else
+                  model_equation1  ="t1 = intercept + ID + dam";
+            end
+
+            R      = 1.0
+            model1 = build_model(model_equation1,R);
+
+            G2 = [1.0 0.5
+                  0.5 1.0]
+            set_random(model1,"ID dam",pedigree,G2);
+
+
+            if single_step == false # genomic models or PBLUP
+                  out1 = runMCMC(model1,phenotypes,estimate_variance=true,heterogeneous_residuals=false,
+                                 double_precision=true,
+                                 chain_length=1000,output_samples_frequency=10,
+                                 printout_frequency=100,seed=314);
+                  results = innerjoin(out1["EBV_t1"], phenotypes, on = :ID)
+                  ind_id  = findall(x -> !ismissing(x), results[!,:t1])
+                  accuracy  = cor(results[ind_id,:EBV],results[ind_id,:t1])
+                  PA_dict[newdir] =  accuracy
+            elseif single_step == true && test_method!="PBLUP" && test_method!="GBLUP"
+                  # genomic model, no PBLUP/GBLUP
+                  out1=runMCMC(model1,phenotypes,estimate_variance=true,heterogeneous_residuals=false,
+                              chain_length=1000,output_samples_frequency=10,printout_frequency=100,
+                              single_step_analysis=true,pedigree=pedigree,seed=314);
+                  results = innerjoin(out1["EBV_t1"], phenotypes, on = :ID)
+                  ind_id  = findall(x -> !ismissing(x), results[!,:t1])
+                  accuracy  = cor(results[ind_id,:EBV],results[ind_id,:t1])
+                  PA_dict[newdir] =  accuracy
+            end
+            # GWAS
+            if test_method != "PBLUP" && test_method!="GBLUP"
+                  gwas1=GWAS("results/MCMC_samples_marker_effects_geno_t1.txt")
+                  show(gwas1)
+                  println()
+                  #gwas2=GWAS("results/MCMC_samples_marker_effects_geno_y1.txt",mapfile,model1)
+                  #show(gwas2)
+            end
+            cd("..")
+
+            printstyled("\n\n\n\n\n\n\n\nTest multi-trait $test_method analysis using $(single_step ? "in" : "")complete genomic data\n\n\n",bold=true,color=:green)
+            # MT analysis
+            newdir = "MT_"*(single_step ? "SS" : "")*test_method*"/"
+            mkdir(newdir)
+            cd(newdir)
+
+            if test_method != "PBLUP"
+                  G3 = [1.0 0.5 0.5
+                        0.5 1.0 0.5
+                        0.5 0.5 1.0]
+                  if single_step
+                        global geno = get_genotypes(genofile_ssbr,G3,header=true,separator=',',method=test_method,estimatePi=test_estimatePi);
+                  else
+                        global geno = get_genotypes(genofile,G3,header=true,separator=',',method=test_method,estimatePi=test_estimatePi);
+                  end
+            end
+
+            if test_method != "PBLUP"
+                  model_equation2 ="t1 = intercept + ID + dam + geno
+                                    t2 = intercept + ID + geno
+                                    t3 = intercept + ID + geno";
+            else
+                  model_equation2 ="t1 = intercept + ID + dam
+                                    t2 = intercept + ID
+                                    t3 = intercept + ID";
+            end
+
+
+            R      = [1.0 0.5 0.5
+                      0.5 1.0 0.5
+                      0.5 0.5 1.0]
+            model2 = build_model(model_equation2,R);
+
+            G2 = [1.0 0.5 0.5 0.5
+                  0.5 1.0 0.5 0.5
+                  0.5 0.5 1.0 0.5
+                  0.5 0.5 0.5 1.0]
+            set_random(model2,"ID dam",pedigree,G2);
+
+
+            if single_step == false
+                  out2=runMCMC(model2,phenotypes,estimate_variance=true,heterogeneous_residuals=false,double_precision=true,
+                              chain_length=1000,output_samples_frequency=10,printout_frequency=100,seed=314);
+                  results = innerjoin(out2["EBV_t1"], phenotypes, on = :ID)
+                  ind_id  = findall(x -> !ismissing(x), results[!,:t1])
+                  accuracy  = cor(results[ind_id,:EBV],results[ind_id,:t1])
+                  PA_dict[newdir] =  accuracy
+            elseif single_step == true && test_method!="PBLUP" && test_method!="GBLUP"
+                  out2=runMCMC(model2,phenotypes,estimate_variance=true,heterogeneous_residuals=false,double_precision=true,
+                              chain_length=1000,output_samples_frequency=10,printout_frequency=100,
+                              single_step_analysis=true,pedigree=pedigree,seed=314);
+                  results = innerjoin(out2["EBV_t1"], phenotypes, on = :ID)
+                  ind_id  = findall(x -> !ismissing(x), results[!,:t1])
+                  accuracy  = cor(results[ind_id,:EBV],results[ind_id,:t1])
+                  PA_dict[newdir] =  accuracy
+            end
+            if test_method != "PBLUP" && test_method!="GBLUP"
+                  gwas1=GWAS("results/MCMC_samples_marker_effects_geno_t1.txt")
+                  show(gwas1)
+                  println()
+                  #gwas2=GWAS(model2,mapfile,"results/MCMC_samples_marker_effects_geno_y1.txt","results/MCMC_samples_marker_effects_geno_y2.txt",genetic_correlation=true)
+                  #show(gwas2)
+            end
+            cd("..")
+      end
+end
+
+#PA_df = DataFrame(PredictionAccuracy = collect(values(PA_dict)), Methods = collect(keys(PA_dict)))
+#CSV.write("./../src/4.Datasets/data/PA_bigdata.csv", PA_df)
