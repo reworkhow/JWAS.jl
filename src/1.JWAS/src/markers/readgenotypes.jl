118de19391a11885371c3119c6eddaf7c653fd36
zhaotianjing <tzhao9@unl.edu>
2024-02-28 13:57:40 -0600

    allow double_precision for genotype; add comment to explain why we cannot use genotype as DataFrame

--- src/1.JWAS/src/markers/readgenotypes.jl
+++ src/1.JWAS/src/markers/readgenotypes.jl
@@ -105,11 +105,13 @@ function get_genotypes(file::Union{AbstractString,Array{Float64,2},Array{Float32
                        estimate_variance = true, estimate_scale = false,
                        constraint = false, #for multi-trait only, constraint=true means no genetic covariance among traits
                        ## format:
-                       separator = ',', header = true,
+                       separator = ',', header = true, double_precision = false,
                        ## quality control:
                        quality_control = true, MAF = 0.01, missing_value = 9.0,
                        ## others:
                        center = true, starting_value = false)
+    #Define data precision
+    data_type = double_precision ? Float64 : Float32 #works for both genotypes and GRM
     #Read the genotype file
     if typeof(file) <: AbstractString  #string (path to a file)
         #print info about the file format
@@ -126,16 +128,19 @@ function get_genotypes(file::Union{AbstractString,Array{Float64,2},Array{Float32
         #set type for each column
         ncol = length(row1)
         etv  = Array{DataType}(undef, ncol)
-        fill!(etv, Float32)
+        fill!(etv, data_type)
         etv[1] = String #individual ID
         close(myfile)
         #read a large genotype file
         data      = CSV.read(file,DataFrame,types=etv,delim = separator,header=false,skipto=(header==true ? 2 : 1))
         obsID     = map(string,data[!,1])
-        genotypes = Matrix{Float32}(data[!,2:end]) #memory usage is doubled here; cannot use readdlm because 1st column is string
+        genotypes = Matrix(data[!,2:end]) #memory usage is doubled here
         #clean memory
         data = nothing
         GC.gc()
+        #Note: why we choose to use `genotypes = Matrix(data[!,2:end])`
+         #- cannot use `genotypes=@view data[!,2:end]` because the view will be copied as a new DataFrame in QC: `genotypes = genotypes[:,select]`
+         #- cannot use `select!(genotypes, Not(1))` to simply delete the 1st column of obsID from the DataFrame, because genotypes must be a Matrix used for matrix multiplication (e.g. EBV += Mi.output_genotypes*Mi.α[traiti]).
     elseif typeof(file) == DataFrames.DataFrame #Datafarme
         println("The first column in the dataframe should be individual IDs.")
         println("The remaining columns are markers with the data type Number.")
@@ -146,14 +151,14 @@ function get_genotypes(file::Union{AbstractString,Array{Float64,2},Array{Float32
             printstyled("The marker IDs are set to 1,2,...,#markers\n",bold=true)
         end
         obsID     = map(string,file[!,1])
-        genotypes = map(Float32,Matrix(file[!,2:end]))
+        genotypes = map(data_type, Matrix(file[!,2:end]))
     elseif typeof(file) <: Union{Array{Float64,2}, Array{Float32,2}, Array{Int64,2}, Array{Int32,2}, Array{Any,2}} #Array (Matrix)
         println("The input data is a genotype matrix, without individual IDs and marker IDs.")
         markerID  = string.(1:size(file,2))
         printstyled("The marker IDs are set to 1,2,...,#markers\n",bold=true)
         obsID     = map(string,string.(1:size(file,1)))
         printstyled("The individual IDs is set to 1,2,...,#observations\n",bold=true)
-        genotypes = map(Float32,convert(Matrix,file))
+        genotypes = map(data_type, convert(Matrix,file))
     else
         error("The data type is not supported.")
     end
@@ -191,7 +196,7 @@ function get_genotypes(file::Union{AbstractString,Array{Float64,2},Array{Float32
     end
 
     markerMeans   = center==true ? center!(genotypes) : mean(genotypes,dims=1) #centering genotypes or not
-    p             = markerMeans/2.0       #allele frequency
+    p             = markerMeans/data_type(2.0)       #allele frequency
 
     #Naive Quality Control 2, minor allel frequency & fixed loci
     if quality_control == true
@@ -210,14 +215,14 @@ function get_genotypes(file::Union{AbstractString,Array{Float64,2},Array{Float32
     if method == "GBLUP"
         if !isGRM #calculate the relationship matrix from the genotype covariate matrix
             genotypes  = genotypes ./ sqrt.(2*p.*(1 .- p))
-            genotypes  = (genotypes*genotypes'+ I*0.00001)/nMarkers
+            genotypes  = (genotypes*genotypes'+ I*data_type(0.00001))/nMarkers
             println("A genomic relationship matrix is computed from genotypes.")
             isGRM  = true
         end
         add_small_value_count = 0
         while isposdef(genotypes) == false
             println("The relationship matrix is not positive definite. A very small number is added to the diagonal.")
-            genotypes = genotypes + I*0.00001
+            genotypes = genotypes + I*data_type(0.00001)
             add_small_value_count += 1
             if add_small_value_count > 10
                 error("Please provide a positive-definite relationship matrix.")
