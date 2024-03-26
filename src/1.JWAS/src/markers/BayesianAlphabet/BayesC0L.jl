5692f5fabdfd46148dddf8de04515667781d1585
zhaotianjing <tzhao9@unl.edu>
2024-03-26 12:49:46 -0500

    fix G.val in BayesL

--- src/1.JWAS/src/markers/BayesianAlphabet/BayesC0L.jl
+++ src/1.JWAS/src/markers/BayesianAlphabet/BayesC0L.jl
@@ -1,25 +1,25 @@
 function megaBayesL!(genotypes,wArray,vare)
     Threads.@threads for i in 1:length(wArray) #ntraits
         BayesL!(genotypes.mArray,genotypes.mRinvArray,genotypes.mpRinvm,
-            wArray[i],genotypes.α[i],genotypes.gammaArray,vare[i,i],genotypes.G[i,i])
+            wArray[i],genotypes.α[i],genotypes.gammaArray,vare[i,i],genotypes.G.val[i,i])
     end
 end
 
 function BayesL!(genotypes,ycorr,vare)
     BayesL!(genotypes.mArray,genotypes.mRinvArray,genotypes.mpRinvm,
-            ycorr,genotypes.α[1],genotypes.gammaArray,vare,genotypes.G)
+            ycorr,genotypes.α[1],genotypes.gammaArray,vare,genotypes.G.val)
 end
 
 function megaBayesC0!(genotypes,wArray,vare)
     Threads.@threads for i in 1:length(wArray) #ntraits
         BayesL!(genotypes.mArray,genotypes.mRinvArray,genotypes.mpRinvm,
-                wArray[i],genotypes.α[i],[1.0],vare[i,i],genotypes.G[i,i])
+                wArray[i],genotypes.α[i],[1.0],vare[i,i],genotypes.G.val[i,i])
     end
 end
 
 function BayesC0!(genotypes,ycorr,vare)
     BayesL!(genotypes.mArray,genotypes.mRinvArray,genotypes.mpRinvm,
-            ycorr,genotypes.α[1],[1.0],vare,genotypes.G)
+            ycorr,genotypes.α[1],[1.0],vare,genotypes.G.val)
 end
 
 function BayesL!(xArray,xRinvArray,xpRinvx,
